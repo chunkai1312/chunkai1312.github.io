ae57d7dd30548d4d7580e3be9a62b6d8a45a5e66
Chun-Kai Wang <chunkai1312@gmail.com>
2019-11-07 16:56:50 +0800

    Updates

---
+++ precache-manifest.b58fe86bc8ba9b3c151b4122ea86db50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b92de725d74a5ff698860c3287c818",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "778bfc631f90281cc080",
+    "url": "/site/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "778bfc631f90281cc080",
+    "url": "/site/static/js/2.0e9ee1aa.chunk.js"
+  },
+  {
+    "revision": "5875460e67e5df680b4f",
+    "url": "/site/static/js/main.0315806f.chunk.js"
+  },
+  {
+    "revision": "5f8460f8a8dc825630c3",
+    "url": "/site/static/js/runtime-main.85d784b5.js"
+  }
+]);
