6f2f9594418c34f4dfebb0921ea6e46b9f7e482b
Chun-Kai Wang <chunkai1312@gmail.com>
2020-05-29 03:08:44 +0800

    refactor: fetch articles

--- src/services/article.ts
+++ src/services/article.ts
@@ -13,16 +13,15 @@ interface Article {
 
 export async function getArticles() {
   const baseUrl = 'https://blog.chunkai.me';
-  const url = `${baseUrl}/api/categories/%E6%8A%80%E8%A1%93%E5%88%86%E4%BA%AB.json`;
+  const url = `${baseUrl}/api/posts.json`;
   const response = await axios.get(url);
 
-  const articles = (response.data.postlist as [Article])
+  const articles = (response.data.data as [Article])
     .map(article => ({
       ...article,
       cover: `${baseUrl}${article.cover}`,
       url: `${baseUrl}/${moment(article.date).format('YYYY/MM/DD')}/${article.slug}`
     }))
-    .reverse()
     .slice(0, 4);
 
   return articles;
